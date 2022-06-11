f5bf822ecf9667b38532351a7c849d3d084505c6
Arturo Salcedo <arturosalcedo@rudo.es>
2022-06-11 09:23:38 +0200

    Add BerriesScreen and ViewModel

---
+++ app/src/main/java/es/rudo/pokedex/presentation/screens/main/berries/BerriesScreen.kt
@@ -0,0 +1,8 @@
+package es.rudo.pokedex.presentation.screens.main.berries
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BerriesScreen() {
+    
+}
---
+++ app/src/main/java/es/rudo/pokedex/presentation/screens/main/berries/BerriesViewModel.kt
@@ -0,0 +1,9 @@
+package es.rudo.pokedex.presentation.screens.main.berries
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class BerriesViewModel: ViewModel() {
+
+}
