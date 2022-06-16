da944b0c05b470305a1ffa96060428430a6aa377
Arturo Salcedo <asalcedo2323@live.com>
2022-06-16 23:10:03 +0200

    Remove title param from NavigationItem

--- app/src/main/java/es/rudo/pokedex/navigation/NavigationItem.kt
+++ app/src/main/java/es/rudo/pokedex/navigation/NavigationItem.kt
@@ -4,10 +4,9 @@ import es.rudo.pokedex.R
 
 sealed class NavigationItem(
     var route: String,
-    var icon: Int,
-    var title: String
+    var icon: Int
 ) {
-    object Berries: NavigationItem("berries", R.drawable.ic_berry, "Berries")
-    object Pokemon: NavigationItem("pokemon", R.drawable.ic_pokeball, "Pokémon")
-    object Items: NavigationItem("items", R.drawable.ic_potion, "Items")
+    object Berries: NavigationItem("berries", R.drawable.ic_berry)
+    object Pokemon: NavigationItem("pokemon", R.drawable.ic_pokeball)
+    object Items: NavigationItem("items", R.drawable.ic_potion)
 }
