8bde427c1dc46833174e77b0e1c49fa9371098b1
Arturo Salcedo <asalcedo2323@live.com>
2023-08-29 19:26:57 +0200

    Update dependencies. Fix bottom navigation bar items ripple

--- app/src/main/java/es/rudo/pokedex/navigation/PokedexBottomNavigationView.kt
+++ app/src/main/java/es/rudo/pokedex/navigation/PokedexBottomNavigationView.kt
@@ -1,36 +1,30 @@
 package es.rudo.pokedex.navigation
 
 import android.content.Context
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Text
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.ripple.RippleAlpha
-import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import es.rudo.pokedex.presentation.theme.ColorRed
 import es.rudo.pokedex.presentation.theme.LightRed
-import java.util.*
 
 @Composable
 fun PokedexBottomNavigationView(
@@ -44,80 +38,66 @@ fun PokedexBottomNavigationView(
         NavigationItem.Items
     )
 
-    CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
-        BottomNavigation(
-            backgroundColor = Color.White,
-            modifier = Modifier.fillMaxWidth()
-        ) {
+    BottomNavigation(
+        backgroundColor = Color.White,
+        modifier = Modifier.fillMaxWidth()
+    ) {
 
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentDestination = navBackStackEntry?.destination
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
 
-            navigationItems.forEach { item ->
-                val selected = currentDestination?.hierarchy?.any { destination ->
-                    destination.route == item.route
-                } == true
-                Spacer(modifier = Modifier.width(20.dp))
-                BottomNavigationItem(
-                    icon = {
-                        Image(
-                            painter = painterResource(id = item.icon),
-                            contentDescription = stringResource(item.title),
-                            modifier = Modifier.size(24.dp)
-                        )
-                    },
-                    label = {
-                        Text(text = context.getString(item.title))
-                    },
-                    alwaysShowLabel = true,
-                    selected = selected,
-                    selectedContentColor = ColorRed,
-                    unselectedContentColor = Color.Black.copy(0.75f),
-                    onClick = {
-                        navController.navigate(item.route) {
-                            // Pop up to the start destination of the graph to
-                            // avoid building up a large stack of destinations
-                            // on the back stack as users select items
-                            navController.graph.startDestinationRoute?.let { route ->
-                                popUpTo(route) {
-                                    saveState = true
-                                }
+        navigationItems.forEach { item ->
+            val selected = currentDestination?.hierarchy?.any { destination ->
+                destination.route == item.route
+            } == true
+            BottomNavigationItem(
+                icon = {
+                    Image(
+                        painter = painterResource(id = item.icon),
+                        contentDescription = stringResource(item.title),
+                        modifier = Modifier.size(24.dp)
+                    )
+                },
+                label = {
+                    Text(text = context.getString(item.title))
+                },
+                alwaysShowLabel = true,
+                selected = selected,
+                selectedContentColor = ColorRed,
+                unselectedContentColor = Color.Black.copy(0.75f),
+                onClick = {
+                    navController.navigate(item.route) {
+                        // Pop up to the start destination of the graph to
+                        // avoid building up a large stack of destinations
+                        // on the back stack as users select items
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
                             }
-                            // Avoid multiple copies of the same destination when
-                            // re-selecting the same item
-                            launchSingleTop = true
-                            // Restore state when re-selecting a previously selected item
-                            restoreState = true
                         }
-                    },
-                    modifier = Modifier
-                        .background(
-                            color = if (selected) LightRed
-                            else Color.White,
-                            shape = CircleShape.copy(CornerSize(20.dp))
-                        )
-                        .border(
-                            border = if (selected) BorderStroke(1.dp, ColorRed)
-                            else BorderStroke(0.dp, Color.White),
-                            shape = CircleShape.copy(CornerSize(20.dp))
-                        )
-                )
-                Spacer(modifier = Modifier.width(20.dp))
-            }
+                        // Avoid multiple copies of the same destination when
+                        // re-selecting the same item
+                        launchSingleTop = true
+                        // Restore state when re-selecting a previously selected item
+                        restoreState = true
+                    }
+                },
+                modifier = Modifier
+                    .background(
+                        color = if (selected) LightRed
+                        else Color.White,
+                    )
+                    .clip(RectangleShape)
+            )
         }
     }
 }
 
-private object NoRippleTheme : RippleTheme {
-    @Composable
-    override fun defaultColor() = Color.Unspecified
-
-    @Composable
-    override fun rippleAlpha(): RippleAlpha =
-        RippleAlpha(
-            0.0f,
-            0.0f,
-            0.0f,
-            0.0f
-        )
+@Preview
+@Composable
+fun PokedexBottomNavigationPreview() {
+    PokedexBottomNavigationView(
+        rememberNavController(),
+        LocalContext.current
+    )
 }
