563263a218b51ef588462d49d13421793ba6cf62
Arturo Salcedo <asalcedo2323@live.com>
2023-09-02 17:31:21 +0200

    Add pokemon front sprite

--- app/src/main/java/es/rudo/pokedex/presentation/screens/main/pokemon/PokemonScreen.kt
+++ app/src/main/java/es/rudo/pokedex/presentation/screens/main/pokemon/PokemonScreen.kt
@@ -7,12 +7,21 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -22,11 +31,14 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -35,6 +47,8 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import es.rudo.domain.model.Language
 import es.rudo.domain.model.Pokemon
 import es.rudo.domain.model.PokemonType
@@ -181,18 +195,41 @@ fun PokemonCard(
         Column(
             modifier = Modifier
                 .padding(
-                    horizontal = dimensionResource(R.dimen.padding_small),
-                    vertical = dimensionResource(R.dimen.padding_regular)
+                    start = dimensionResource(R.dimen.padding_small),
+                    top = dimensionResource(R.dimen.padding_small),
+                    end = dimensionResource(R.dimen.padding_small),
+                    bottom = dimensionResource(R.dimen.padding_regular)
                 ),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            PokemonSprites(
+                pokemonSprites = pokemon.sprites,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(text = pokemonName)
+            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
             PokemonTypesRow(
                 pokemonTypes = pokemonTypes,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
         }
     }
+}
+
+@Composable
+fun PokemonSprites(
+    pokemonSprites: ArrayList<String?>?,
+    modifier: Modifier
+) {
+    AsyncImage(
+        model = pokemonSprites?.first(),
+        contentDescription = null,
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .defaultMinSize(minHeight = 125.dp)
+    )
 }
 
 @Composable
