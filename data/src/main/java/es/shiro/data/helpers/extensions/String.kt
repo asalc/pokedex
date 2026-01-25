8474401af094bc40521c1aa930b81184085d1f9d
Arturo Salcedo <asalcedo2323@live.com>
2026-01-25 23:08:43 +0100

    Refactor tests

---
+++ data/src/main/java/es/shiro/data/helpers/extensions/String.kt
@@ -0,0 +1,4 @@
+package es.shiro.data.helpers.extensions
+
+val String.Companion.EMPTY_STRING: String
+    get() = ""
---
+++ data/src/test/kotlin/es/shiro/pokedex/data/mocks/GenericMocks.kt
@@ -0,0 +1,14 @@
+package es.shiro.pokedex.data.mocks
+
+import es.shiro.data.helpers.extensions.EMPTY_STRING
+import es.shiro.data.model.remote.GenericRemoteDto
+
+internal val nullGenericRemoteDto = GenericRemoteDto()
+internal val emptyGenericRemoteDto = GenericRemoteDto(
+    name = String.EMPTY_STRING,
+    url = String.EMPTY_STRING
+)
+internal val genericRemoteDto = GenericRemoteDto(
+    name = "name",
+    url = "url"
+)
